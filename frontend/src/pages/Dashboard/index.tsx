b774fe86758540e439aa955368a377cbbf3e8deb
Guilherme Fonseca <guilhermefonsecamarquez@gmail.com>
2021-05-08 17:35:00 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -5,32 +5,32 @@ import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function Dashboard() {
-    return (
-        <>
-            <NavBar></NavBar>
-            <div className="container">
-                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-                <div className="row px-3">
-                    <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
-                        <BarChart></BarChart>
-                    </div>
-                    <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Todas Vendas</h5>
-                        <DonutChart></DonutChart>
-                    </div>
-                </div>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart></BarChart>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <DonutChart></DonutChart>
+          </div>
+        </div>
 
-                <div className="py-3">
-                    <h2 className="text-primary">Todas Vendas</h2>
-                </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
 
-                <DataTable></DataTable>
-            </div>
-            <Footer></Footer>
-        </>
-    );
+        <DataTable></DataTable>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default Dashboard;
